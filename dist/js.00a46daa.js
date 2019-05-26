eac765567637e25aa237983ebf9b8c54da7cfd64
Britton Hayes <46035482+brittonhayes@users.noreply.github.com>
2019-05-26 15:37:06 -0700

    removed node modules from git

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -3159,7 +3159,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "27785" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59978" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
